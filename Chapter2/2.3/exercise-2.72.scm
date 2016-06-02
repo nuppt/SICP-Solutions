70e086a192f1f10edde36fd0025e2f3b83f40de7
nuppt <nuppt@mail.ustc.edu.cn>
2016-06-02 20:18:30 +0800

    prove the property of special huffman tree

---
+++ Chapter2/2.3/exercise-2.72.scm
@@ -0,0 +1,5 @@
+;; Q1: The order of growth of the number of steps needed to encode the most frequent symbol
+;; A1: T(n) = O(n)
+
+;; Q2: The order of growth of the number of steps needed to encode the least frequent symbol
+;; A2: T(n) = n + n-1 + n-2 + ... + 1 = O(n^2)
